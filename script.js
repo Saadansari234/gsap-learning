a1f30b5422b6fd0c561ac365ad50b515e4d55842
Saadansari234 <saadansari3212@gmail.com>
2024-09-08 18:31:26 +0530

    learn gsap animation on cursor and onmouseenter and onmouseout'

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-let svg = document.querySelector("svg")
-let inital = `M 10 100 Q 250 100 490 100`
+let cursor= document.querySelector("#cursor")
+let main= document.querySelector("#main")
+let image= document.querySelector("#image")
+main.addEventListener("mousemove",(dent)=>{
+  gsap.to(cursor,{
+    x:dent.x,
+    y:dent.y,
+    ease: "back.out(1.7)",
+    duration:1,
+  })
 
-svg.addEventListener("mousemove", (dest) => {
-    let cordinates = `M 10 100 Q ${dest.x} ${dest.y} 490 100`
-    gsap.to("svg path", {
-        attr: { d: cordinates },
-        duration:0.3,
-        ease:"power3.out"
+})
+
+image.addEventListener("mouseenter",(dent)=>{
+    cursor.innerHTML=`<p>view more<p>`
+  gsap.to(cursor,{
+      scale:4,
     })
 })
 
-svg.addEventListener("mouseleave", () => {
-    gsap.to("svg path", {
-        attr: { d: inital },
-        ease:"elastic.out(1,0.3)",
-        duration:0.3
-    })
-
-})
+image.addEventListener("mouseout",(dent)=>{
+     cursor.innerHTML=""
+    gsap.to(cursor,{
+        scale:1,
+      })
+  })
